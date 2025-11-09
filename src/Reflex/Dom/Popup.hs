406c89eec487cb8db4d60722ff999fa2690e8d74
card <cardenaso11@users.noreply.github.com>
2025-11-09 16:29:03 +0000

    add combinators for relative offsets

--- src/Reflex/Dom/Popup.hs
+++ src/Reflex/Dom/Popup.hs
@@ -10,6 +10,24 @@
 module Reflex.Dom.Popup(
   PopupConfig(..),
   popup,
+  defaultPopupGap,
+  pBelow,
+  pBelowWithGap,
+  pAbove,
+  pAboveWithGap,
+  pRight,
+  pRightWithGap,
+  pLeft,
+  pLeftWithGap,
+  pBottomLeft,
+  pBottomLeftWithGap,
+  pBottomRight,
+  pBottomRightWithGap,
+  pTopLeft,
+  pTopLeftWithGap,
+  pTopRight,
+  pTopRightWithGap,
+  pCentered
 )
 where
 
@@ -32,6 +50,7 @@ import Reflex.Dom.Core
       DomBuilderSpace,
       RawElement,
       Dynamic,
+      constDyn,
       mapMaybe,
       Event, ffor
     )
@@ -56,9 +75,9 @@ data PopupConfig t m = PopupConfig
   , _popupConfig_zIndex :: Dynamic t (Maybe Int)
   -- ^ z-index CSS property for the popup interior div. If Nothing, sets it to 1000
   , _popupConfig_topGapFromContainer :: Dynamic t (Maybe T.Text)
-  -- ^ Gap from the top of the popup container to the top of the popup interior. You can use any units. Default is Nothing, which means 0 gap.
+  -- ^ Gap from the top of the popup container to the top of the popup interior. You can use any CSS math expression. Default is Nothing, which means 0 gap.
   , _popupConfig_leftGapFromContainer :: Dynamic t (Maybe T.Text)
-  -- ^ Gap between the leftmost of the popup container and the leftmost of the popup interior. You can use any units. Default is Nothing, which means 0 gap.
+  -- ^ Gap between the leftmost of the popup container and the leftmost of the popup interior. You can use any CSS math expression. Default is Nothing, which means 0 gap.
   }
 
 instance Reflex t => Default (PopupConfig t m) where
@@ -120,3 +139,79 @@ popup cfg widget = do
     let pressedEsc :: Event t ()
         pressedEsc = keydown Escape el
     pure (pressedEsc, a)
+
+-- Default gap between popup container and interior. 8px (2 * 4px) is a common base spacing.
+defaultPopupGap :: T.Text
+defaultPopupGap = "8px"
+
+setTopOffset :: Reflex t => Dynamic t (Maybe T.Text) -> PopupConfig t m -> PopupConfig t m
+setTopOffset top cfg = cfg { _popupConfig_topGapFromContainer = top }
+
+setLeftOffset :: Reflex t => Dynamic t (Maybe T.Text) -> PopupConfig t m -> PopupConfig t m
+setLeftOffset left cfg = cfg { _popupConfig_leftGapFromContainer = left }
+
+pBelow :: Reflex t => PopupConfig t m -> PopupConfig t m
+pBelow = pBelowWithGap $ pure defaultPopupGap
+
+pBelowWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pBelowWithGap gapD = setTopOffset (fmap Just calcedBelow)
+  where
+    calcedBelow = ffor gapD $ \gap -> T.concat ["calc(100% + ", gap, ")"]
+
+pAbove :: Reflex t => PopupConfig t m -> PopupConfig t m
+pAbove = pAboveWithGap $ pure defaultPopupGap
+
+pAboveWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pAboveWithGap gapD = setTopOffset (fmap Just calcedAbove)
+  where
+    calcedAbove = ffor gapD $ \gap -> T.concat ["calc(-100% - ", gap, ")"]
+
+pRight :: Reflex t => PopupConfig t m -> PopupConfig t m
+pRight = pRightWithGap $ pure defaultPopupGap
+
+pRightWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pRightWithGap gapD = setLeftOffset (fmap Just calcedRight)
+  where
+    calcedRight = ffor gapD $ \gap -> T.concat ["calc(100% + ", gap, ")"]
+
+pLeft :: Reflex t => PopupConfig t m -> PopupConfig t m
+pLeft = pLeftWithGap $ pure defaultPopupGap
+
+pLeftWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pLeftWithGap gapD = setLeftOffset (fmap Just calcedLeft)
+  where
+    calcedLeft = ffor gapD $ \gap -> T.concat ["calc(-100% - ", gap, ")"]
+
+pBottomLeft :: Reflex t => PopupConfig t m -> PopupConfig t m
+pBottomLeft = pBottomLeftWithGap $ pure defaultPopupGap
+
+pBottomLeftWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pBottomLeftWithGap gapD = pLeftWithGap gapD . pBelowWithGap gapD
+
+pBottomRight :: Reflex t => PopupConfig t m -> PopupConfig t m
+pBottomRight = pBottomRightWithGap $ pure defaultPopupGap
+
+pBottomRightWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pBottomRightWithGap gapD = pRightWithGap gapD . pBelowWithGap gapD
+
+pTopLeft :: Reflex t => PopupConfig t m -> PopupConfig t m
+pTopLeft = pTopLeftWithGap $ pure defaultPopupGap
+
+pTopLeftWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pTopLeftWithGap gapD = pLeftWithGap gapD . pAboveWithGap gapD
+
+pTopRight :: Reflex t => PopupConfig t m -> PopupConfig t m
+pTopRight = pTopRightWithGap $ pure defaultPopupGap
+
+pTopRightWithGap :: Reflex t => Dynamic t T.Text -> PopupConfig t m -> PopupConfig t m
+pTopRightWithGap gapD = pRightWithGap gapD . pAboveWithGap gapD
+
+pCentered :: (Reflex t, Applicative m) => PopupConfig t m -> PopupConfig t m
+pCentered cfg =
+  setLeftOffset (pure (Just "50%")) -- Set top of popup to center of container
+    . setTopOffset (pure (Just "50%")) -- Set leftmost of popup to center of container
+    $ cfg { _popupConfig_interiorAttrs = centeredStyles : _popupConfig_interiorAttrs cfg }
+  where
+    centeredStyles = "style" ~: ["transform" ~:: "translate(-50%, -50%)"]
+    -- Now shift the popup back another 50% of its own width and height to actually center it
+    -- If we didn't do this, it'd have its topleft at the center, instead of actually being centered
