4e3548aab39df44f34a0ca136e655bec6a78dd67
card <cardenaso11@users.noreply.github.com>
2025-11-09 12:56:37 +0000

    allow configuring top and left gaps between popup interior & container

--- src/Reflex/Dom/Popup.hs
+++ src/Reflex/Dom/Popup.hs
@@ -33,7 +33,7 @@ import Reflex.Dom.Core
       RawElement,
       Dynamic,
       mapMaybe,
-      Event
+      Event, ffor
     )
 import Reflex.Dom.Attrs
 
@@ -41,6 +41,8 @@ import Reflex.Dom (ffor2)
 import qualified Data.Text as T
 import Data.Default (Default(def))
 import JSDOM.Types (MonadJSM)
+import Control.Applicative (liftA3)
+import Data.Maybe (fromMaybe)
 
 data PopupConfig t m = PopupConfig
   { _popupConfig_toggleVisibility :: Dynamic t Bool
@@ -53,6 +55,10 @@ data PopupConfig t m = PopupConfig
   -- ^ Convenience field that adds extra attributes to the popup container div, which has class popup-exterior
   , _popupConfig_zIndex :: Dynamic t (Maybe Int)
   -- ^ z-index CSS property for the popup interior div. If Nothing, sets it to 1000
+  , _popupConfig_topGapFromContainer :: Dynamic t (Maybe T.Text)
+  -- ^ Gap from the top of the popup container to the top of the popup interior. You can use any units. Default is Nothing, which means 0 gap.
+  , _popupConfig_leftGapFromContainer :: Dynamic t (Maybe T.Text)
+  -- ^ Gap between the leftmost of the popup container and the leftmost of the popup interior. You can use any units. Default is Nothing, which means 0 gap.
   }
 
 instance Reflex t => Default (PopupConfig t m) where
@@ -62,6 +68,8 @@ instance Reflex t => Default (PopupConfig t m) where
     , _popupConfig_interiorAttrs = mempty
     , _popupConfig_containerAttrs = mempty
     , _popupConfig_zIndex = pure Nothing
+    , _popupConfig_topGapFromContainer = pure Nothing
+    , _popupConfig_leftGapFromContainer = pure Nothing
     }
 
 popup
@@ -85,13 +93,19 @@ popup cfg widget = do
       attrsExterior =
         ("style" ~: ["position" ~:: "relative"]) :  _popupConfig_containerAttrs cfg
 
+      configCombinedDyn :: Dynamic t (Bool, Maybe Int, Maybe T.Text, Maybe T.Text)
+      configCombinedDyn = (,,,)
+        <$> _popupConfig_toggleVisibility cfg
+        <*> _popupConfig_zIndex cfg
+        <*> _popupConfig_topGapFromContainer cfg
+        <*> _popupConfig_leftGapFromContainer cfg
       attrsInterior :: [Attrs t m]
       attrsInterior =
         ("style" ~:
-        ffor2 (_popupConfig_toggleVisibility cfg) (_popupConfig_zIndex cfg) (\isVisible zIndex->
+        ffor configCombinedDyn (\(isVisible,zIndex, topGap, leftGap)->
             [ "position" ~:: "absolute"
-            , "top" ~:: "0"
-            , "left" ~:: "0"
+            , "top" ~:: fromMaybe "0" topGap
+            , "left" ~:: fromMaybe "0" leftGap
             , "z-index" ~:: maybe "1000" T.show zIndex
             , case (isVisible, _popupConfig_hiddenOrNone cfg) of
               (True, _) -> mconcat ["visibility" ~:: "visible", "display" ~:: "block"]
